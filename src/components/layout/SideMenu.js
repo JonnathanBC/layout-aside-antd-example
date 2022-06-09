f1168d95e7f1f485ef0d4bdb713fee3eb205b8d8
jonnathan <jonnabcl56@gmail.com>
2022-06-09 18:44:50 -0500

    Main Layout v3 (Overriding style classes + Component SideMenu + menu  )

--- src/components/layout/SideMenu.js
+++ src/components/layout/SideMenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import logo from '../../logo.svg'
+import { menuItems } from '../../config/menus'
 
 const { Sider } = Layout
 
@@ -8,6 +9,16 @@ export const SideMenu = () => {
   return (
     <Sider>
       <img src={logo} alt={'Test Logo'} />
+      <Menu>
+        {menuItems.map(menu => {
+          const { icons, label, path } = menu
+          return (
+            <Menu.Item key={path} icon={icons}>
+              {label}
+            </Menu.Item>
+          )
+        })}
+      </Menu>
     </Sider>
   )
 }
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Layout } from 'antd'
 import { SideMenu } from './SideMenu'
+import './index.css'
 
 const { Content, Footer, Header } = Layout
 
 export const MainLayout = () => {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <SideMenu />
-      <Layout>
-        <Header>Header Layout</Header>
-        <Content>Content Layout</Content>
-        <Footer>Footer Layout</Footer>
+    <aside>
+      <Layout style={{ minHeight: '100vh' }}>
+        <SideMenu />
+        <Layout>
+          <Header>Header Layout</Header>
+          <Content>Content Layout</Content>
+          <Footer>Footer Layout</Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </aside>
   )
 }
